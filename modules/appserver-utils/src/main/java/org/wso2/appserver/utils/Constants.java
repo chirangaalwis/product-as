b2bae8dfd4d0f2239ee8a672e7669ea7ea2c1591
chirangaalwis <chirangaalwis@gmail.com>
2016-02-08 23:38:07 +0530

    Commiting the pom and PathUtils.java of AppServerUtils.

---
+++ modules/appserver-utils/src/main/java/org/wso2/appserver/utils/Constants.java
@@ -0,0 +1,30 @@
+/*
+ *  Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.wso2.appserver.utils;
+
+/**
+ * This class defines WSO2 Application Server specific utility constants.
+ *
+ * @since 6.0.0
+ */
+public class Constants {
+    //  Environmental variable property name constant
+    public static final String CATALINA_BASE = "catalina.base";
+    //  File path related constants
+    protected static final String WSO2AS_CONFIG_FILE_NAME = "wso2as-web.xml";
+    protected static final String TOMCAT_CONFIGURATION_FOLDER_NAME = "conf";
+    protected static final String WSO2_CONFIGURATION_FOLDER_NAME = "wso2";
+}
